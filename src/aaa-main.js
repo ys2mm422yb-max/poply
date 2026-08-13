817b9c08df266477c9007ff1c9176b9bcb0aeb2f
ys2mm422yb-max <ys2mm422yb@privaterelay.appleid.com>
2026-08-14 01:22:02 +0200

    Wire clean AAA app entry

--- src/aaa-main.js
+++ src/aaa-main.js
@@ -1,1 +1,12 @@
+import { ASSETS } from './aaa-view.js';
 import { createUI } from './aaa-ui.js';
+import { installDrag } from './aaa-drag.js';
+
+document.documentElement.style.setProperty('--poply-hero',`url(${ASSETS.hero})`);
+document.documentElement.style.setProperty('--poply-atlas',`url(${ASSETS.atlas})`);
+const root=document.querySelector('#app');
+const toast=document.querySelector('#toast');
+const ghost=document.querySelector('#drag-ghost');
+const ui=createUI(root,toast);
+installDrag({root,ghost,ui});
+ui.render();
