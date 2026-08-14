a00b91aa56a08b2a5dcb4141a0904badc8d8db6f
ys2mm422yb-max <ys2mm422yb@privaterelay.appleid.com>
2026-08-14 04:48:46 +0200

    Allow authored AAA motion module in shell contract

--- tests/aaa-shell.test.js
+++ tests/aaa-shell.test.js
@@ -13,5 +13,5 @@ test('live shell has one AAA entry and no legacy V2 layer stack',async()=>{
 
 test('AAA stylesheet entry only composes authored modules',async()=>{
   const css=(await read('src/aaa.css')).trim();
-  assert.equal(css,"@import './aaa-shell.css';\n@import './aaa-board.css';\n@import './aaa-views.css';");
+  assert.equal(css,"@import './aaa-shell.css';\n@import './aaa-board.css';\n@import './aaa-views.css';\n@import './aaa-motion.css';");
 });
