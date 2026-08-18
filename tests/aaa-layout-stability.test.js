52445f19c45aaa772ae523d4821bce71f9ab563c
ys2mm422yb-max <ys2mm422yb@privaterelay.appleid.com>
2026-08-18 15:33:24 +0200

    Advance layout shell contract to gameplay4

--- tests/aaa-layout-stability.test.js
+++ tests/aaa-layout-stability.test.js
@@ -7,8 +7,8 @@ const read=path=>readFile(new URL(path,root),'utf8');
 
 test('layout foundation is followed by hierarchy and gameplay-first release layers',async()=>{
   const [html,main]=await Promise.all([read('index.html'),read('src/aaa-main.js')]);
-  assert.match(html,/aaa-layout-stability\.css\?v=20260818-layout1[^<]*"><link rel="stylesheet" href="\.\/src\/aaa-ui-hierarchy\.css\?v=20260818-hierarchy1[^<]*"><link rel="stylesheet" href="\.\/src\/aaa-ui-hierarchy-active\.css\?v=20260818-hierarchy4[^<]*"><link rel="stylesheet" href="\.\/src\/aaa-gameplay-first\.css\?v=20260818-gameplay1[^<]*"><link rel="stylesheet" href="\.\/src\/aaa-gameplay-first-orders\.css\?v=20260818-gameplay2[^<]*"><link rel="stylesheet" href="\.\/src\/aaa-gameplay-first-polish\.css\?v=20260818-gameplay3/);
-  assert.match(html,/aaa-main\.js\?v=20260818-gameplay3/);
+  assert.match(html,/aaa-layout-stability\.css\?v=20260818-layout1[^<]*"><link rel="stylesheet" href="\.\/src\/aaa-ui-hierarchy\.css\?v=20260818-hierarchy1[^<]*"><link rel="stylesheet" href="\.\/src\/aaa-ui-hierarchy-active\.css\?v=20260818-hierarchy4[^<]*"><link rel="stylesheet" href="\.\/src\/aaa-gameplay-first\.css\?v=20260818-gameplay1[^<]*"><link rel="stylesheet" href="\.\/src\/aaa-gameplay-first-orders\.css\?v=20260818-gameplay2[^<]*"><link rel="stylesheet" href="\.\/src\/aaa-gameplay-first-polish\.css\?v=20260818-gameplay4/);
+  assert.match(html,/aaa-main\.js\?v=20260818-gameplay4/);
   assert.match(main,/installLayoutStability/);
   assert.match(main,/installGameplayFirst/);
 });
